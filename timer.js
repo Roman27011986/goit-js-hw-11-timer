60aed1d06fa8f4c8822e6e6607412fe63787fe2e
Roman <68850770+Roman27011986@users.noreply.github.com>
2021-01-06 00:12:52 +0200

    fix

--- timer.js
+++ timer.js
@@ -1,39 +1,10 @@
-// const daysRef = document.querySelector('.value[data-value="days"]');
-// const hoursRef = document.querySelector('.value[data-value="hours"]');
-// const minsRef = document.querySelector('.value[data-value="mins"]');
-// const secsRef = document.querySelector('.value[data-value="secs"]');
-// const nextDate = new Date('jan 27, 2021');
 
-// setInterval(() => {
-//   const currentDate = Date.now();
-//   const dataTime = nextDate - currentDate;
-//   updateTime(dataTime);
-// }, 1000);
-
-// function updateTime(time) {
-//   const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-
-//   const hours = pad(
-//     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-//   );
-
-//   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-
-//   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-//   daysRef.textContent = days;
-//   hoursRef.textContent = hours;
-//   minsRef.textContent = mins;
-//   secsRef.textContent = secs;
-// }
-
-// function pad(value) {
-//   return String(value).padStart(2, '0');
-// }
 
 class CountdownTimer {
   constructor({ selector, targetDate }) {
-    this.selector = selector;
+    this.selector = document.querySelector(selector);
     this.targetDate = targetDate;
+    this.start()
   }
 
   start() {
@@ -63,8 +34,8 @@ class CountdownTimer {
   }
 }
 const timer = new CountdownTimer({
-  selector: document.querySelector('#timer-1'),
+  selector: '#timer-1',
   targetDate: new Date('jan 27, 2021'),
 });
 
-timer.start();
+
